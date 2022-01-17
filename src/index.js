228b675738f3a1a2f6de75468138bfbf2dc473ed
mishalapa <mishamedme@gmail.com>
2022-01-17 17:50:14 +0300

    fix: add reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+   number = Number(Math.abs(number).toString().split('').reverse().join(''));
+   return number;
 }
+
+
+
